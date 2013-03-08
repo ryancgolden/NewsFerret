e0249538bdbda269c3b5cb6e276ad1e3b2d78313
ryan.golden@oracle.com <ryan.golden@oracle.com>
2013-03-08 10:37:34 -0600

    Removing some clutter MATLAB files

--- src/matlab/myFile.m
+++
@@ -1,6 +0,0 @@
-load startTime
-disp(['I started learning MATLAB on ' startString]);
-disp(['Just some test code']);
-disp((['hotfix change']);
-disp(['Added on test branch']);
-disp(['Added another line on test branch']);
